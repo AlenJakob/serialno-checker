1b57e3bb25890f4a850e4e4c26259975874bf88a
alen <grafuxa@gmail.com>
2021-01-29 13:37:49 +0100

    ux-ver2

--- utils/footer.js
+++ utils/footer.js
@@ -1,14 +1,14 @@
 export const footer = `<footer class="footer ">
 <div class="content has-text-centered">
-    <p class="is-text-small">
-        <strong class="has-text-info is-text-small">2021</strong> Created by <b class="has-text-info is-text-small">Alen Jakob</b>
+    <p class="is-size-6">
+        <strong class="has-text-info is-size-6">2021</strong> Created by <b class="has-text-info is-size-6">Alen Jakob</b>
     </p>
     <p class=" is-size-6">check
-        <a class="has-text-black is-size-6" href="https://github.com/AlenJakob"><i class="is-size-6 fab fa-github-alt fa-lg"></i>
+        <a class="has-text-black is-size-6" href="https://github.com/AlenJakob"><i class="fab fa-github-alt fa-lg"></i>
             My
-            Github</a> / <a href="https://www.linkedin.com/in/alen-jakob/"><i class="is-size-6 fab fa-linkedin fa-lg"></i>
+            Github</a> / <a  class="is-size-6" href="https://www.linkedin.com/in/alen-jakob/"><i class=" fab fa-linkedin fa-lg"></i>
             Linkedin</a> or just contact me
-        via email <a class="is-size-6" href="mailto:alenjakob@gmail.com">alenjakob@gmail.com
+        via email <a class="is-size-6 " href="mailto:alenjakob@gmail.com">alenjakob@gmail.com
         </a>
     </p>
 </div>
