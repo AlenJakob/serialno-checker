7b02b1f33e8cf7074a0b604487255cdffc6a9e5b
alen <grafuxa@gmail.com>
2021-01-29 13:10:29 +0100

    added new styles

--- utils/footer.js
+++ utils/footer.js
@@ -1,15 +1,15 @@
-export const footer = `<footer class="footer">
+export const footer = `<footer class="footer ">
 <div class="content has-text-centered">
-    <p>
-        <strong class="has-text-info">2021</strong> Created by <b class="has-text-info">Alen Jakob</b>
+    <p class="is-text-small">
+        <strong class="has-text-info is-text-small">2021</strong> Created by <b class="has-text-info is-text-small">Alen Jakob</b>
     </p>
-    <p>check
-
-        <a class="has-text-black" href="https://github.com/AlenJakob"><i class=" fab fa-github-alt fa-lg"></i>
+    <p class=" is-size-6">check
+        <a class="has-text-black is-size-6" href="https://github.com/AlenJakob"><i class="is-size-6 fab fa-github-alt fa-lg"></i>
             My
-            Github</a> / <a href="https://www.linkedin.com/in/alen-jakob/"><i class="fab fa-linkedin fa-lg"></i>
+            Github</a> / <a href="https://www.linkedin.com/in/alen-jakob/"><i class="is-size-6 fab fa-linkedin fa-lg"></i>
             Linkedin</a> or just contact me
-        via email <a href="mailto:alenjakob@gmail.com">alenjakob@gmail.com</a>
+        via email <a class="is-size-6" href="mailto:alenjakob@gmail.com">alenjakob@gmail.com
+        </a>
     </p>
 </div>
 </footer>`;
