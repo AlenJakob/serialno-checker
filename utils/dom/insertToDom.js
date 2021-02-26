b6cc0971d551d88685d29fd6122cca5ad035a9e2
alen <grafuxa@gmail.com>
2021-02-26 12:29:30 +0100

    export data with only serial no. external

--- utils/dom/insertToDom.js
+++ utils/dom/insertToDom.js
@@ -1,14 +1,27 @@
 export function insertListDom(domList, arrayList, getFullDate) {
-  console.log(arrayList);
-  return arrayList.forEach((el, i) => {
+  arrayList.forEach((el, i) => {
+    // console.log(el.id);
+
     domList.innerHTML += `
-        <li class="list-item item" data-id="${el.id}"><b>${i + 1}. </b> ${
+        <li class="list-item item"><b>${i + 1}. </b> ${
       el.serial
     }  <i class="${getFullDate(
       el.serial.substr(5, 4)
     )} fas fa-check-square"></i>
-          <i class="delete" style="float:right";></i>
+          <i  data-id="${el.id}" class="delete" style="float:right";></i>
           </li>
       `;
+  });
+
+  const listItem = document.querySelectorAll("#list li");
+  listItem.forEach((e) => {
+    //  if(e.target){
+    e.addEventListener("click", (e) => {
+      // console.log(e.target);
+      if (e.target.classList.contains("delete")) {
+        console.log(e.target.getAttribute("data-id"));
+      }
+    });
+    //  }
   });
 }
--- utils/export.js
+++ utils/export.js
@@ -3,6 +3,15 @@ const exportBtn = document.querySelector("#exportBtn");
 exportBtn.addEventListener("click", () => {
   const fileNameVal = document.querySelector("#fileName").value;
   let dropHistory = JSON.parse(localStorage.getItem("Serial_List")) || [];
-  var xls = new XlsExport([...dropHistory], "list");
+  // var xls = new XlsExport([...dropHistory], "list");
+  // retrieve a array of object with properties "serial"
+  let values = dropHistory.map((a) => {
+    return { serial: a.serial };
+  });
+
+  var xls = new XlsExport([...values], "list");
+
+  console.log(values);
+  // xls.exportToXLS(fileNameVal);
   xls.exportToXLS(fileNameVal);
 });
