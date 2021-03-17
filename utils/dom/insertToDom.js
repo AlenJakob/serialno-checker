9e63a235df36f86bc1422b8d7576a1bff6af3e38
alen <grafuxa@gmail.com>
2021-03-17 15:16:59 +0100

    fix removing single item

--- utils/dom/insertToDom.js
+++ utils/dom/insertToDom.js
@@ -1,4 +1,5 @@
 import { getFullDate } from "../validateDate";
+
 export function insertListDom(domList, arrayList, getFullDate) {
   arrayList.forEach((el, i) => {
     // console.log(el.id);
@@ -13,24 +14,23 @@ export function insertListDom(domList, arrayList, getFullDate) {
           </li>
       `;
   });
-
   function removeElemFromList(ListWithRemovedItem, id) {
-    let newArr = ListWithRemovedItem.filter((item) => {
-      return item.id != id;
-    });
-    console.log(newArr);
-    localStorage.setItem("Serial_List", JSON.stringify(newArr));
+    ListWithRemovedItem = ListWithRemovedItem.filter((obj) => obj.id !== id);
+    localStorage.setItem("Serial_List", JSON.stringify(ListWithRemovedItem));
   }
 
-  let list = JSON.parse(localStorage.getItem("Serial_List")) || [];
   const listItem = document.querySelectorAll("#list li");
 
+  // let list = JSON.parse(localStorage.getItem("Serial_List")) || [];
   listItem.forEach((el) => {
     el.addEventListener("click", (e) => {
       if (e.target.classList.contains("delete")) {
         let id = e.target.getAttribute("data-id");
         e.target.parentElement.remove();
-        removeElemFromList(list, id);
+        removeElemFromList(
+          JSON.parse(localStorage.getItem("Serial_List")) || [],
+          id
+        );
       }
     });
   });
